c84bdcbf7f0d594c90571f41e66f8687feede7f0
hrishikesh-patil <hrishikesh.patil@yandex.com>
2018-08-22 11:15:12 +0530

    Update EightPuzzle.java

--- EightPuzzle.java
+++ EightPuzzle.java
@@ -1,24 +1,24 @@
 import java.util.*;
-class State{
+class Node{
 	public int mat[][] = new int[3][3];
-	public ArrayList<State> visited = new ArrayList<State>();
-	public State(int m[][]){
+	public ArrayList<Node> visited = new ArrayList<Node>();
+	public Node(int m[][]){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				mat[i][j] = m[i][j];
 			}
 		}
 	}
-    public State(State s){
-        visited = new ArrayList<State>(s.visited);
+    public Node(Node s){
+        visited = new ArrayList<Node>(s.visited);
         for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				mat[i][j] = s.mat[i][j];
 			}
 		}
     }
-    public State(){}
-	public boolean compareState(State s){
+    public Node(){}
+	public boolean compareNode(Node s){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				if(s.mat[i][j] != mat[i][j]) return false;
@@ -26,22 +26,22 @@ class State{
 		}
 		return true;
 	}
-    public boolean checkIfVisited(State state){
+    public boolean checkIfVisited(Node Node){
 		boolean result = false;
-		for(State s: visited){
-            if(s.compareState(state)) result = true;
+		for(Node s: visited){
+            if(s.compareNode(Node)) result = true;
 		}
 		return result;
 	}
 }	
 public class EightPuzzle{
 	public static void main(String args[]){		
-		State initial = new State(new int[][]{
+		Node initial = new Node(new int[][]{
 					{1, 8, 2},
 					{4, 6, 3},
 					{7, 5, 0}
 				});				
-		State goal = new State(new int[][]{
+		Node goal = new Node(new int[][]{
 					{1, 2, 3},
 					{4, 5, 6},
 					{7, 8, 0}
@@ -49,16 +49,16 @@ public class EightPuzzle{
 		SteepestAscent(initial, goal);	
 	}
     
-	public static void SteepestAscent(State initState, State goalState){
-        State temp = initState;
+	public static void SteepestAscent(Node initNode, Node goalNode){
+        Node temp = initNode;
         System.out.println("Solution Path:");
-        while(!temp.compareState(goalState)){
+        while(!temp.compareNode(goalNode)){
             int currCost = 0;
-			State[] s = getPossibleStates(temp);
+			Node[] s = getPossibleNodes(temp);
             for(int i=0; i<s.length; i++){
-			    if(getCost(s[i], goalState) > currCost && !s[i].checkIfVisited(s[i])){
+			    if(getCost(s[i], goalNode) > currCost && !s[i].checkIfVisited(s[i])){
                     temp = s[i];
-                    currCost = getCost(s[i], goalState);
+                    currCost = getCost(s[i], goalNode);
                 }
             }
             System.out.println("Cost : " + currCost + " " + Arrays.deepToString(temp.mat));
@@ -66,7 +66,7 @@ public class EightPuzzle{
         }
         System.out.println("Found : " + Arrays.deepToString(temp.mat));
 	}
-	public static int getCost(State s, State goal){
+	public static int getCost(Node s, Node goal){
 		int quality = 0;
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
@@ -75,39 +75,39 @@ public class EightPuzzle{
 		}
 		return quality;
 	}
-	public static State[] getPossibleStates(State s){
-		State[] result = new State[4];
+	public static Node[] getPossibleNodes(Node s){
+		Node[] result = new Node[4];
 		int[] coord = getBlankSpace(s);
 		int x = coord[0], y = coord[1], i = 0;
 		if(x-1 < 3 && x-1 >=0){
-			result[i] = swappedState(s, x, y, x-1, y);
+			result[i] = swappedNode(s, x, y, x-1, y);
 			i++;
 		}
 		if(x+1 < 3 && x+1 >= 0){
-			result[i] = swappedState(s, x, y, x+1, y);			
-            i++;
+			result[i] = swappedNode(s, x, y, x+1, y);			
+            		i++;
 		}
 		if(y+1 < 3 && y+1 >= 0){
-			result[i] = swappedState(s, x, y, x, y+1);
-            i++;
+			result[i] = swappedNode(s, x, y, x, y+1);
+            		i++;
 		}		
 		if(y-1 < 3 && y-1 >= 0){
-			result[i] = swappedState(s, x, y, x, y-1);
-            i++;
+			result[i] = swappedNode(s, x, y, x, y-1);
+		        i++;
 		}
         
-        //resizing the array
-        State r[] = Arrays.copyOf(result, i);
+        	//resizing the array
+        	Node r[] = Arrays.copyOf(result, i);
         
 		return r;
 	}
-	public static State swappedState(State s, int  x, int y, int toX, int toY){
-		State temp = new State(s);
+	public static Node swappedNode(Node s, int  x, int y, int toX, int toY){
+		Node temp = new Node(s);
 		temp.mat[x][y] = s.mat[toX][toY];
 		temp.mat[toX][toY] = 0;
 		return temp;
 	}
-	public static int[] getBlankSpace(State s){
+	public static int[] getBlankSpace(Node s){
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				if(s.mat[i][j] == 0) return new int[]{i, j};
